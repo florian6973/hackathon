2d37ef8ada9f4871b1f0f2b9ed5728f6ba271da9
Florent POLLET <ap.ap@club-internet.fr>
2021-01-29 09:30:30 +0100

    pg

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-import pygame
+import pygame as pg
 
 def main():    
     # initialize the pygame module
-    pygame.init()
+    pg.init()
     # load and set the logo
     #logo = pygame.image.load("logo32x32.png")
     #pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
+    pg.display.set_caption("minimal program")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
+    screen = pg.display.set_mode((240,180))
      
     # define a variable to control the main loop
     running = True
@@ -17,11 +17,14 @@ def main():
     # main loop
     while running:
         # event handling, gets all event from the event queue
-        for event in pygame.event.get():
+        for event in pg.event.get():
             # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
+            if event.type == pg.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT:
+                    print("left")
 
 
 
