ab413b15a82cf9dde95c93224f6a77aa4f400d94
Jean-Edouard Dupau <jean-edouard.dupau@mines-paristech.fr>
2021-01-29 12:16:31 +0100

    ajout ennemi

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from character import *
 def main():    
     player = Player("Robin")
     jeu = Jeu()
+    ennemi = Evil("JE", 100, 1, 5, 5)
     player.rect.x = jeu.taille_case
     player.rect.y = jeu.taille_case
     
@@ -13,10 +14,12 @@ def main():
     # main loop
     while running:
         pg.time.wait(50)
-        jeu.afficher(player)
+        jeu.afficher(player, ennemi)
         player.rentrer_mur(jeu.map.map[player.coordonnees_y + player.direction[1], player.coordonnees_x + player.direction[0]])
-
         player.move(jeu.taille_case)
+        l = [jeu.map.map(ennemi.coordonnees_x + 1, ennemi_coordonnees_y), jeu.map.map(ennemi.coordonnees_x - 1, ennemi_coordonnees_y), jeu.map.map(ennemi.coordonnees_x, ennemi_coordonnees_y - 1), jeu.map.map(ennemi.coordonnees_x, ennemi_coordonnees_y + 1)]
+        ennemi.move(player, jeu.taille_case, l)
+
         # event handling, gets all event from the event queue
         for event in pg.event.get():
             # only do something if the event is of type QUIT
