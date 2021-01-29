972471755894d55b97da66a8895cc19f685d8207
Jean-Edouard Dupau <jean-edouard.dupau@mines-paristech.fr>
2021-01-29 10:08:42 +0100

    commit class jeu

--- src/jeu.py
+++ src/jeu.py
@@ -3,20 +3,19 @@ import pygame as pg
 from map import Map
 class Jeu:
     def __init__(self):
-        self.map = Map(100,50)
+        self.map = Map(100, 50)
         self.taille_x = 100
         self.taille_y = 50
         pg.init()
         pg.display.set_caption("minimal program")
-        self.font = pygame.font.SysFont(None, 16)
-        img = font.render('hello', True, BLUE)
+        self.font = pg.font.SysFont(None, 16)
         self.screen = pg.display.set_mode((1600, 800))
         self.taille_case = 16
     def afficher(self):
-        for i in range(100):
-            for j in range(50):
-                img = self.font.render(self.map.map[i, j], (255, 255, 255))
-                self.screen.blit(img)
+        for i in range(50):
+            for j in range(100):
+                img = self.font.render(self.map.map[i, j], True, (255, 255, 255))
+                self.screen.blit(img,  (self.taille_case * j , self.taille_case *i ))
 
 
                 
