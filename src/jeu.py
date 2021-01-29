fa701cd4167f5889329bbb080f251cf21fb2b9b1
Jean-Edouard Dupau <jean-edouard.dupau@mines-paristech.fr>
2021-01-29 10:22:32 +0100

    class joueur

--- src/jeu.py
+++ src/jeu.py
@@ -3,20 +3,23 @@ import pygame as pg
 from map import Map
 class Jeu:
     def __init__(self):
-        self.map = Map(100, 50)
-        self.taille_x = 100
-        self.taille_y = 50
+        self.taille_x = 50
+        self.taille_y = 25
+        self.map = Map(self.taille_x, self.taille_y)
+        
+        for k in range(10):
+            self.map.map[k, 20] = "|" 
         pg.init()
         pg.display.set_caption("minimal program")
-        self.font = pg.font.SysFont(None, 16)
-        self.screen = pg.display.set_mode((1600, 800))
+        self.font = pg.font.SysFont("Comic Sans MS", 16)
+        self.screen = pg.display.set_mode((16 * self.taille_x, 16*self.taille_y))
         self.taille_case = 16
     def afficher(self):
-        for i in range(50):
-            for j in range(100):
+        for i in range(self.taille_y):
+            for j in range(self.taille_x):
                 img = self.font.render(self.map.map[i, j], True, (255, 255, 255))
                 self.screen.blit(img,  (self.taille_case * j , self.taille_case *i ))
-
+        pg.display.flip()
 
                 
 
