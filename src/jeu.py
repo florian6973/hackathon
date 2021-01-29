bfd32351c67aad80796390f1ad66677f4e78814c
Jean-Edouard Dupau <jean-edouard.dupau@mines-paristech.fr>
2021-01-29 10:46:12 +0100

    main + jeu

--- src/jeu.py
+++ src/jeu.py
@@ -1,24 +1,26 @@
 import numpy as np
 import pygame as pg
 from map import Map
+import character
 class Jeu:
     def __init__(self):
         self.taille_x = 50
         self.taille_y = 25
         self.map = Map(self.taille_x, self.taille_y)
+        self.map.load("map0.rg")
         
-        for k in range(10):
-            self.map.map[k, 20] = "|" 
         pg.init()
         pg.display.set_caption("minimal program")
         self.font = pg.font.SysFont("Comic Sans MS", 16)
         self.screen = pg.display.set_mode((16 * self.taille_x, 16*self.taille_y))
         self.taille_case = 16
-    def afficher(self):
+    def afficher(self, player):
+        pg.draw.rect(self.screen, (0,0,0), (0, 0, self.taille_x * self.taille_case, self.taille_y * self.taille_case))
         for i in range(self.taille_y):
             for j in range(self.taille_x):
                 img = self.font.render(self.map.map[i, j], True, (255, 255, 255))
                 self.screen.blit(img,  (self.taille_case * j , self.taille_case *i ))
+        self.screen.blit(player.image, player.rect)
         pg.display.flip()
 
                 
--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
 import pygame as pg
 from jeu import Jeu
+from character import *
 
 def main():    
+    player = Player("Robin")
+    player.rect.x = 0
+    player.rect.y = 0
     jeu = Jeu()
     running = True
      
     # main loop
     while running:
-        jeu.afficher()
+        jeu.afficher(player)
         # event handling, gets all event from the event queue
         for event in pg.event.get():
             # only do something if the event is of type QUIT
             if event.type == pg.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT:
+                    player.rect.x -= jeu.taille_case
+                if event.key == pg.K_RIGHT:
+                    player.rect.x += jeu.taille_case
+                if event.key == pg.K_UP:
+                    player.rect.y -= jeu.taille_case
+                if event.key == pg.K_DOWN:
+                    player.rect.y += jeu.taille_case
 
 
 
